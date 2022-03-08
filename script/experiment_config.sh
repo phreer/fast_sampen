653c27ddd5c7962a06b3cf0e6468a7bcf749ad63
Weifeng Liu <iphreeliu@gmail.com>
2022-03-08 13:53:05 +0800

    [Style] Format experiment_n0n1.

--- script/experiment_config.sh
+++ script/experiment_config.sh
@@ -6,4 +6,10 @@ input_files=(chfdb/chf01.txt\
              ltstdb/s20011.txt\
              mghdb/mgh001.txt\
              chbmit/chb07_01.txt\
-             mit-bih-long-term-ecg-database-1.0.0/14046.txt)
+             mit-bih-long-term-ecg-database-1.0.0/14046.txt\
+             RRAF/AF_fa002.rr_multirecord.txt\
+             RRHealth/Health_Filt-time-1583.01.rr_multirecord.txt\
+             RRCHF/CHF_Filt-time-9643.rr_multirecord.txt\
+             gaussian/gaussian-1m.txt\
+             pink/pink-1m.txt\
+             uniform/uniform-1m.txt)
--- script/run_ex_convergence_n0n1_grid.sh
+++ script/run_ex_convergence_n0n1_grid.sh
@@ -5,10 +5,11 @@ set -o noclobber
 DoExperimentConvergenceSampleSize()
 {
     local filename=$1
-    local m=$2
-    local r=$3
-    local line_offset=$4
-    local output_file=$5
+    local n=$2
+    local m=$3
+    local r=$4
+    local line_offset=$5
+    local output_file=$6
     date >> $output_file
     ./build/bin/experiment_n0n1 \
         --input $filename \
@@ -16,8 +17,8 @@ DoExperimentConvergenceSampleSize()
         --input-type double \
         --line-offset $line_offset \
         -n $n -m $m -r $r \
-        --swr \
-        --variance \
+        --swr -u -q \
+        --variance --random \
         --output-level 1 >> $output_file
 }
 
@@ -28,18 +29,22 @@ if [ ! -e $CONFIG ]; then
     exit -1
 fi
 source $CONFIG
-subdir=convergence_grid_final_m${m}_r${r}_211013
+set -x
+n=500000
+r=0.15
+m=4
+subdir=convergence_n0n1_grid/m${m}_r${r}_220308
 
 if [ ! -e result/$subdir ]; then
     mkdir -p result/$subdir
 fi
-comment="QMC using lattice sequence."
+comment="Experiment on the convergence of sample size (N_0) and sample number (N_1)"
 echo $comment > result/${subdir}/convergence.txt
 for f in ${input_files[@]}; do
     input_file="$INPUT_DIR"/$f
     echo "input_file: $input_file"
     database=${input_file%/*}
     database=${input_file##*/}
-    output_file=result/${subdir}/convergence_r${r}_m${m}_${database}_$(date +%Y-%m-%d).txt 
-    DoExperimentConvergenceSampleSize $input_file $m $r $line_offset $output_file &
+    output_file=result/${subdir}/convergence_r${r}_m${m}_${database}.txt 
+    DoExperimentConvergenceSampleSize $input_file $n $m $r $line_offset $output_file &
 done 
