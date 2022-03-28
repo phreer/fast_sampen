9325421a40d8ed94580e63cc6d3172eef913f0dc
Weifeng Liu <iphreeliu@gmail.com>
2022-03-28 13:38:07 +0800

    [Feature] Add scripts for experiments on memory.

--- script/quick_test_fast_sampen.sh
+++ script/quick_test_fast_sampen.sh
@@ -2,7 +2,8 @@
 build/bin/fast_sampen -m 3 -n 10003 -r 0.15 \
   --input-format multirecord \
   --input data.PhysioNet/chfdb/chf01.txt  \
-  --sample-size 2000 --sample-num 1 \
-  --line-offset 100000 \
-  --kdtree-sample -rkd --simple-kdtree \
+  --sample-size 2000 --sample-num 10 \
+  --line-offset 100000 --random \
+  --swr \
   --output-level 1
+#  --kdtree-sample -rkd --simple-kdtree \
--- script/run_ex_memory.sh
+++ script/run_ex_memory.sh
@@ -6,7 +6,7 @@ DoExperimentMemoryPolicy2() {
   local r=$2
   local m=$3
   local output_dir=$4
-  for i in `seq 4 20`; do
+  for i in `seq 4 14`; do
     local n=$(python -c "print((2 ** $i) * 1024)")
     local n0n1_str=$(python3 "$SCRIPT_DIR"/get_n0n1_policy2.py $n)
     IFS=" " read -r -a n0n1_array <<< $n0n1_str
@@ -19,23 +19,25 @@ DoExperimentMemoryPolicy2() {
   done
 }
 
-input_files=(chfdb/chf01.txt\
-             ltafdb/00.txt\
-             ltstdb/s20011.txt\
-             mghdb/mgh001.txt\
-             chbmit/chb07_01.txt\
-             mit-bih-long-term-ecg-database-1.0.0/14046.txt\
-             RRAF/AF_fa002.rr_multirecord.txt\
-             RRHealth/Health_Filt-time-1583.01.rr_multirecord.txt\
-             RRCHF/CHF_Filt-time-9643.rr_multirecord.txt\
-             gaussian/gaussian-1m.txt\
-             pink/pink-1m.txt\
-             uniform/uniform-1m.txt)
+# input_files=(chfdb/chf01.txt\
+#              ltafdb/00.txt\
+#              ltstdb/s20011.txt\
+#              mghdb/mgh001.txt\
+#              chbmit/chb07_01.txt\
+#              mit-bih-long-term-ecg-database-1.0.0/14046.txt\
+#              RRAF/AF_fa002.rr_multirecord.txt\
+#              RRHealth/Health_Filt-time-1583.01.rr_multirecord.txt\
+#              RRCHF/CHF_Filt-time-9643.rr_multirecord.txt\
+#              gaussian/gaussian-1m.txt\
+#              pink/pink-1m.txt\
+#              uniform/uniform-1m.txt)
 
+input_files=(ltstdb/s20011.txt)
 m=4
 r=0.15
+tag=220328
 INPUT_DIR="$SCRIPT_DIR"/../data.PhysioNet
-OUTPUT_DIR=result/memory_220326/policy2
+OUTPUT_DIR=result/memory_${tag}/policy2_m$m
 for input_file in ${input_files[@]}; do
   input_filename=${INPUT_DIR}/$input_file
   DoExperimentMemoryPolicy2 $input_filename $r $m $OUTPUT_DIR &
